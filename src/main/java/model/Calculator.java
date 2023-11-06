04952bfa193e7808a8d7b18345c8f633c55e49a1
Paolo Falco <paolo.falcoapp@gmail.com>
2023-11-06 15:57:04 +0100

    commit 1 - add Calculator and TestCalculator

---
+++ src/main/java/model/Calculator.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+    }
+    public static float add(float num1, float num2){
+        return num1 + num2;
+    }
+
+    public static float subtract(float num1, float num2){
+        return num1 - num2;
+    }
+
+    public static float divide(float num1, float num2){
+        return num1 / num2;
+    }
+
+    public static float multiply(float num1, float num2){
+        return num1 * num2;
+    }
+}
--- src/test/java/model/CalculatorTest.java
+++ src/test/java/model/CalculatorTest.java
@@ -1,4 +1,43 @@
 package model;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 public class CalculatorTest {
+
+    @Test
+    @DisplayName("Control Sum")
+    void TestSum() {
+        float result = Calculator.add(2, 2);
+        assertEquals(4, result);
+        assertNotEquals(5, result);
+    }
+
+    @Test
+    @DisplayName("Control Subtract")
+    void TestSubtract() {
+        float result = Calculator.subtract(2, 2);
+        assertEquals(0, result);
+        assertNotEquals(4, result);
+    }
+
+    @Test
+    @DisplayName("Control Multiply")
+    void TestMultiply() {
+        float result = Calculator.multiply(2, 2);
+        assertEquals(4, result);
+        assertNotEquals(5, result);
+    }
+
+    @Test
+    @DisplayName("Control Divide")
+    void TestDivide() {
+        float result = Calculator.divide(2, 2);
+        assertEquals(1, result);
+        assertNotEquals(4, result);
+    }
 }
+
